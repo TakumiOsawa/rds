0eec0e9322cb927d272e55925883c467b7b0134b
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-08-06 20:56:14 +0900

    fixing application.yml

--- src/main/java/com/sample/aws/rds/config/RdsConfig.java
+++ src/main/java/com/sample/aws/rds/config/RdsConfig.java
@@ -1,11 +1,7 @@
 package com.sample.aws.rds.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
 
 @Configuration
-@EnableRdsInstance(
-        dbInstanceIdentifier = "${rds.identifier}",
-        password ="${rds.password}")
 public class RdsConfig {
 }
